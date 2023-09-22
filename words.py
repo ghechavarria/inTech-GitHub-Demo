f53251af825e0e3abfa4cf8267f02921f16a6cc8
Grace Hechavarria <gracefullsoul19@gmail.com>
2023-09-22 18:16:07 -0400

    deleted punct and added color output

--- words.py
+++ words.py
@@ -1,13 +1,27 @@
 # Program to sort alphabetically the words from a string provided by the user
+
+#pip install termcolor
+from termcolor import colored
+
+#define punctuation
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
 #take input from a user
 my_str = input("Enter a string: ")
 
+#remove punctuation
+no_punct = ""
+for char in my_str:
+    if char not in punctuations:
+        no_punct = no_punct + char
+
 #breakdown the string into a list of words
-words = [word.lower() for word in my_str.split()]
+words = [word.lower() for word in no_punct.split()]
 
 #sort the list
 words.sort()
 
 #print the sorted words
 for word in words:
-    print(word)
+    #print with colored words
+    print(colored(word, 'blue'))
