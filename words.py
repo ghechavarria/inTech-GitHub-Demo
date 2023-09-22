3072c83c6d1637c558bb3938f3340395744289f9
Grace Hechavarria <gracefullsoul19@gmail.com>
2023-09-22 18:04:42 -0400

    sorted string from user

--- words.py
+++ words.py
@@ -1,1 +1,13 @@
 # Program to sort alphabetically the words from a string provided by the user
+#take input from a user
+my_str = input("Enter a string: ")
+
+#breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+#sort the list
+words.sort()
+
+#print the sorted words
+for word in words:
+    print(word)
